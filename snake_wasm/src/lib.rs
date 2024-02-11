13bc40a503d4a780b0a5e4d20e15038db62cd4a0
Vladlan <lanvlad911@gmail.com>
2024-02-11 23:26:20 +0100

    Create World struct

--- snake_wasm/src/lib.rs
+++ snake_wasm/src/lib.rs
@@ -30,3 +30,21 @@ extern {
 
 // to rebuild code run: (note you will have to restart web server to see changes)
 // wasm-pack build --target web 
+
+#[wasm_bindgen]
+pub struct World {
+    width: usize
+}
+
+#[wasm_bindgen]
+impl World {
+    pub fn new(width: usize) -> World {
+        World {
+            width
+        }
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+}
--- snake_wasm/www/index.js
+++ snake_wasm/www/index.js
@@ -1,6 +1,9 @@
-import init, { greet } from "snake_game";
+import init, { greet, World } from "snake_game";
 
 init().then(_ => {
-  greet("V1234");
+  // greet("V1234");
   console.log("OK! 123");
+
+  const world = World.new(16);
+  console.log('world: ', world.width());
 })
