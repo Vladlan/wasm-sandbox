f2b8f42b703510f1227fa449a13a7fd343afeeed
Vladlan <lanvlad911@gmail.com>
2024-02-11 21:27:03 +0100

    add console.log usage from rust

--- snake_wasm/src/lib.rs
+++ snake_wasm/src/lib.rs
@@ -3,5 +3,27 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
-    println!("Hello, {}!", name);
+    let name = format!("name: {}", name);
+    alert(&name);
+
+    log(&name);
 }
+
+#[wasm_bindgen]
+extern {
+   pub fn alert(name: &str);
+}
+
+// the same for console.log
+#[wasm_bindgen(js_namespace = console)]
+extern {
+    #[wasm_bindgen(js_name = log)]
+    pub fn log(s: &str);
+
+    // Optionally, bind multiple arguments or other console methods
+    #[wasm_bindgen(js_name = error)]
+    pub fn error(s: &str);
+}
+
+// to rebuild code run: (note you will have to restart web server to see changes)
+// wasm-pack build --target web 
--- snake_wasm/www/index.js
+++ snake_wasm/www/index.js
@@ -1,6 +1,6 @@
 import init, { greet } from "snake_game";
 
 init().then(_ => {
-  greet("Filip");
-  console.log("OK!");
+  greet("V1234");
+  console.log("OK! 123");
 })
